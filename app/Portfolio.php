02f8cea4db1584910e28d6e81767f2949a3ed0de
Lukas Yelle <lxy5611@g.rit.edu>
2019-10-01 00:54:32 -0400

    Adds preliminary attempt at computed portfolio values

--- app/Portfolio.php
+++ app/Portfolio.php
@@ -6,10 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portfolio extends Model
 {
+    protected $appends = ['value'];
+    protected $fillable = ['user_id', 'platform_data_id'];
+
+    public function getValueAttribute()
+    {
+        $stocks = $this->stocks;
+        return $stocks->value * $stocks->pivot->shares;
+    }
 
     public function stocks()
     {
-        return $this->belongsToMany(Stock::class);
+        return $this->belongsToMany(Stock::class)->withPivot('shares');
     }
 
     public function user()
