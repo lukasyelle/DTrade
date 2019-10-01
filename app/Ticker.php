baf3f8507dbd8fb417051dd3fe858e96d0d2d660
Lukas Yelle <lxy5611@g.rit.edu>
2019-09-30 20:26:20 -0400

    Fixes ticker updating issues

--- app/Ticker.php
+++ app/Ticker.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Ticker extends Model
 {
@@ -12,14 +13,23 @@ class Ticker extends Model
     public function __construct($symbol = null)
     {
         parent::__construct();
-
+        if ($symbol != null) {
+            $table = DB::table('tickers');
+            if ($table->where('symbol', $symbol)->get()->isEmpty()) {
+                $table->insertGetId([
+                        'symbol'    => $symbol,
+                        'data'      => json_encode([]),
+                    ]);
+            }
+        }
         $this->market = new Market();
-        $this->updateData();
+        return $this;
     }
 
     public function updateData()
     {
-        $updatedAt = $this->getUpdatedAtColumn();
+        $updatedAtColumn = $this->getUpdatedAtColumn();
+        $updatedAt = $this->$updatedAtColumn;
         $currentTime = $this->freshTimestamp();
         if ($updatedAt == null || $currentTime->diffInMinutes($updatedAt) > 30) {
             $this->data = $this->market->realTime($this->symbol);
