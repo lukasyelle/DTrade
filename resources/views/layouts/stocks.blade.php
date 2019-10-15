d6be9e0ecda7fa93e718cfa7ed8d1ce13cff5e04
Lukas Yelle <lxy5611@g.rit.edu>
2019-10-14 23:11:11 -0400

    Adds another graphing library for (possible) future use

--- resources/views/layouts/stocks.blade.php
+++ resources/views/layouts/stocks.blade.php
@@ -2,6 +2,8 @@
 
 @section('head')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/4.3.0/echarts.min.js" charset="utf-8"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.css">
     <link rel="stylesheet" href="{{ mix('/css/stocks.css') }}">
 @endsection
 
