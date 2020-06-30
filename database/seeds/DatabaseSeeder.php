67ee4c7e195b10db25e354b8ec6494537f0c6ade
Lukas Yelle <lxy5611@g.rit.edu>
2020-06-30 17:29:53 -0500

    Adds Permission seeder to database seeding entry point. Also ensures that duplicate permissions are not made.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(PermissionsSeeder::class);
         $this->call(StocksSeeder::class);
     }
 }
