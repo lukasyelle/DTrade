7e8ad3c0ac4ce0cbf1fd66e2e4cf2c057eec756a
Lukas Yelle <lxy5611@g.rit.edu>
2020-07-09 15:42:23 +0000

    Apply fixes from StyleCI

--- app/Traits/ProfitabilityAnalysis.php
+++ app/Traits/ProfitabilityAnalysis.php
@@ -7,17 +7,15 @@ trait ProfitabilityAnalysis
     use StockIndicators;
     use StockAnalysis;
 
-    public function makeProjectionFor($profitWindow, $numberDays) {
-
+    public function makeProjectionFor($profitWindow, $numberDays)
+    {
     }
 
     public function testAccuracy($numberDays = 1, $numberTests = 500)
     {
-
     }
 
     public function runTest()
     {
-
     }
 }
