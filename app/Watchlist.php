b52c452debf73628c258e843d92b84ae1aea00c9
Lukas Yelle <lxy5611@g.rit.edu>
2020-07-31 00:35:11 +0000

    Apply fixes from StyleCI

--- app/Watchlist.php
+++ app/Watchlist.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Watchlist extends Model
 {
-    public function stocks() {
+    public function stocks()
+    {
         return $this->belongsToMany(Stock::class);
     }
 }
