97d108730351f5811690f5b2cbf410efc151b849
Lukas Yelle <lxy5611@g.rit.edu>
2020-08-02 01:16:33 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/AutomationController.php
+++ app/Http/Controllers/AutomationController.php
@@ -2,17 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AutomationController extends Controller
 {
     public function enable()
     {
-
     }
 
     public function disable()
     {
-
     }
 }
