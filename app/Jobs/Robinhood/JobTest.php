14eb1f827fc4ea15ad90cc5f2a842da87dc72995
Lukas Yelle <lxy5611@g.rit.edu>
2020-08-04 02:57:12 -0400

    Greatly improves structure and functionality of the stock order task

--- app/Jobs/Robinhood/JobTest.php
+++ app/Jobs/Robinhood/JobTest.php
@@ -12,19 +12,19 @@ class JobTest extends BrowserJob
     public function setup()
     {
         $orderDetails = [
-            'order'     => 'buy',
-            'order_type'=> 'stop limit',
-            'ticker'    => 'wft',
-            'shares'    => '1',
-            'price'     => '0.45',
-            'stop_price'=> '0.5',
-            'expiration'=> 'Good till Canceled',
+            'order'         => 'buy',
+            'order_type'    => 'stop limit',
+            'ticker'        => 'dse',
+            'shares'        => '1',
+            'limit_price'   => '0.5',
+            'stop_price'    => '0.45',
+            'expiration'    => 'Good till Canceled',
         ];
 
         $this->debug = true;
         $this->addTasks([
             new Tasks\LoginTask(),
-            new Tasks\StockSearchTask('aapl'),
+            new Tasks\StockOrderTask($orderDetails),
             new Tasks\LogoutTask(),
         ]);
     }
--- app/Jobs/Robinhood/Tasks/StockOrderTask.php
+++ app/Jobs/Robinhood/Tasks/StockOrderTask.php
@@ -5,6 +5,7 @@ namespace App\Jobs\Robinhood\Tasks;
 use App\Jobs\BrowserTask;
 use App\User;
 use Laravel\Dusk\Browser;
+use \Exception;
 
 class StockOrderTask extends BrowserTask
 {
@@ -33,55 +34,115 @@ class StockOrderTask extends BrowserTask
         }
     }
 
+    public function typeStopPrice(Browser $browser)
+    {
+        $browser->type("input[name='stopPrice']", $this->params['stop_price']);
+    }
+
+    public function typeLimitPrice(Browser $browser)
+    {
+        $browser->type('input[name="limitPrice"]', $this->params['limit_price']);
+    }
+
+    public function typeShares(Browser $browser)
+    {
+        $browser->type("input[name='quantity']", $this->params['shares']);
+    }
+
+    public function selectExpiration(Browser $browser)
+    {
+        $browser->click('button.-aVQMh2t1ihTiMYRyjQA2')
+                ->clickLink($this->params['expiration'],'._39ED64wnAoahW3-2WXpEzU');
+    }
+
+    public function handleMarketOrder(Browser $browser)
+    {
+        $browser->click('button.-aVQMh2t1ihTiMYRyjQA2')
+                ->clickLink('Shares', '._2P2xUGg0JyC-C4gA3ucexl');
+        $this->typeShares($browser);
+    }
+
+    public function handleLimitOrder(Browser $browser)
+    {
+        $this->addRequiredParams('limit_price');
+        $this->typeLimitPrice($browser);
+        $this->typeShares($browser);
+        $this->selectExpiration($browser);
+    }
+
+    public function handleStopLossOrder(Browser $browser)
+    {
+        $this->addRequiredParams('stop_price');
+        $this->typeStopPrice($browser);
+        $this->typeShares($browser);
+        $this->selectExpiration($browser);
+    }
+
+    public function handleStopLimitOrder(Browser $browser)
+    {
+        $this->addRequiredParams(['limit_price', 'stop_price']);
+        $this->typeStopPrice($browser);
+        $this->typeLimitPrice($browser);
+        $this->typeShares($browser);
+        $this->selectExpiration($browser);
+    }
+
+    /**
+     * handleOrder - parse the order type to a handler and execute it
+     *
+     * @param Browser $browser
+     * @param         $orderType
+     *
+     * @throws Exception
+     */
+    public function handleOrder(Browser $browser, $orderType)
+    {
+        $orderHandler = 'handle' . str_replace(' ', '', ucwords($orderType)) . 'Order';
+        if (method_exists($this, $orderHandler)) {
+            $this->$orderHandler($browser);
+        } else {
+            throw new Exception("Order Handler $orderHandler does not exist.");
+        }
+    }
+
+    public function selectOrderType(Browser $browser, $orderType)
+    {
+        // Choose the correct order; Buy or Sell
+        $buyOrSell = ucfirst($this->params['order']);
+        $symbol = strtoupper($this->params['ticker']);
+        $orderLink = $buyOrSell.' '.$symbol;
+
+        if ($browser->seeLink($orderLink)) {
+            $browser->clickLink($orderLink) // Click either Buy or Sell
+                    ->pause(250); // Wait for menu
+        }
+
+        $browser->click('._3C94FbH6M2syYjD4BhzwBF.css-1tw7eno.e1c2vvtl0') // Click the carrot menu
+                ->waitForText('Order Type') // Wait for the dialog to appear
+                ->clickLink(ucwords("$orderType Order"), '._2_dmPaZj4NEDskSalJmSIh._1hH9lEYaPCvkiULeVG3Jjr[role="button"] span') // Click on the correct order
+                ->pause(500); // Wait for menu
+    }
+
     /**
      * @param User    $user
      * @param Browser $browser
      *
      * @throws \Facebook\WebDriver\Exception\TimeOutException
+     * @throws Exception
      */
     public function execute(User $user = null, Browser $browser = null)
     {
-        // Go to the stock page using the Stock Search Task
         $ticker = strtoupper($this->params['ticker']);
-        (new StockSearchTask($ticker))->execute($browser, $browser);
-
-        $order = ucfirst($this->params['order']);
         $orderType = $this->params['order_type'];
 
-        $browser->assertUrlIs("https://robinhood.com/stocks/$ticker")
+        $browser->visit("https://robinhood.com/stocks/$ticker")
                 ->waitForText($ticker);
 
-        $orderLink = $order.' '.$ticker;
-        if ($browser->seeLink($orderLink)) {
-            $browser->clickLink($orderLink); // Click the appropriate order button
-        }
+        $this->selectOrderType($browser, $orderType);
 
-        $browser->type("input[name='quantity']", $this->params['shares']);  // Type the quantity for the order
-
-        if ($orderType != 'market') {
-            // Non-market orders have more fields than regular orders, make sure they are set in the params
-            $this->addRequiredParams(['price', 'expiration']);
-
-            if ($orderType == 'stop limit') {
-                // Stop limit orders require the stop price to be sent too
-                $this->addRequiredParams('stop_price');
-                $browser->type("input[name='stop_price']", $this->params['stop_price']);
-            }
-
-            // Click the dot menu and select the appropriate order. Then enter the necessary information
-            $browser->click('._1sTI87VcHv4IA7ntSiCczw') // Click the dot menu
-                    ->waitForText('Order Type') // Wait for the dialog to appear
-                    ->clickLink(ucwords("$orderType Order")) // Click on the correct order
-                    ->pause(1000) // Wait for the form to adjust
-                    ->clear("input[name='price']")
-                    ->type("input[name='price']", $this->params['price']) // Type the base trade price
-                    ->click('.Select-value') // Select the right expiration option for the order
-                    ->clickLink($this->params['expiration'], 'div') // It is very possible this will not work.. Just saying.
-                    ->waitForText($this->params['expiration'])
-                    ->pause(25000);
-        }
+        $this->handleOrder($browser, $orderType);
 
         // Done filling out the order, try to submit it!
-//            $this->submitOrder($browser, $user, $order);
+        $this->submitOrder($browser, $user, ucfirst($this->params['order']));
     }
 }
